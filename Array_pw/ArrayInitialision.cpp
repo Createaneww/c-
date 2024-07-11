ee52efd73f8a2268b02b93254a906b5e9a04580d
Createaneww <162044593+Createaneww@users.noreply.github.com>
2024-07-11 10:02:05 +0530

    Add files via upload

---
+++ Array_pw/ArrayInitialision.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[6] = {12,5,4,32,7,3};
+    for(int i = 0 ; i<=5 ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+//    reverse  order
+    for(int i = 5 ; i>=0 ; i--){
+        cout<<arr[i]<<" ";
+    }
+
+
+
+
+
+
+
+
+
+}    
+
+
