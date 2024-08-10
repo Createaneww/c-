14406f1f1a58586a56e3ce1d8a2a7ebd9d84061f
Parth Patel <162044593+Createaneww@users.noreply.github.com>
2024-08-10 11:22:56 +0530

    Add files via upload

---
+++ Pw_Basics_c++/loops.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+   // for loop
+    // for(char i = 65;i<=90;i=i+2){
+    //     cout<<i<<endl;
+    // }
+   // While loop
+    // int i = 1;
+    // while(i<=18){
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // int i = 1;
+    // do{
+    //     cout<<i<<endl;
+    //     i++;
+    // }
+    // while(i<=19);
+    
+
+    for(char i = 65;i<=90;i=i+1){
+        cout<<i<<" "<<(int)i<<endl;
+
+    }
+return 0;
+}
