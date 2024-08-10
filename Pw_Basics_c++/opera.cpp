bf6e9cae7c5eebec9fb9bd5b62e85c0834465a8f
Parth Patel <162044593+Createaneww@users.noreply.github.com>
2024-08-10 11:23:11 +0530

    Add files via upload

---
+++ Pw_Basics_c++/opera.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+    cout<<"Enter the value of a :\n";
+    cin>>a;
+    cout<<"enter the value of b :\n";
+    cin>>b;
+    cout<<"The result of relations are :"<<(a==b)||(a<b);
+    return 0;
+}
