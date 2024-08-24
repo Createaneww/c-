6483a4adceb4de1200956c83ff54a3e76f61cf32
Parth Patel <162044593+Createaneww@users.noreply.github.com>
2024-08-24 17:07:33 +0530

    Add files via upload

---
+++ String_PW/Indexing_in_String.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s = "Parthy Patel";
+    // cout<< s[0]; // print any character
+    cout<<s<<endl;
+    s[2] = 'i';  //Update any character
+    s[4] = 'k';
+    cout<<s<<endl;
+return 0;
+}
