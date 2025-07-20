fba7592fd9f7a07881f529976b125e1eb3383c2a
Parth Patel <creatinganewway@gmail.com>
2025-07-20 16:00:34 +0530

    bubble sort revised

---
+++ CPP_practice_basics/Sorting/Bubble.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Bubble_sort(int arr[], int n)
+{
+    int didswap = 0;
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                didswap = 1;
+            }
+        }
+
+        if (didswap == 0)
+        {
+            break;
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of arr you want to sort" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    Bubble_sort(arr, n);
+    cout << "Your sorted arr using selection sort" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
