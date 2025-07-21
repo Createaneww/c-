569bf9d489de338590619af917e230af05804f79
Parth Patel <creatinganewway@gmail.com>
2025-07-21 16:30:16 +0530

    hash map
    revision of hashmaps

--- CPP_practice_basics/Hashing/hash_1.cpp
+++ CPP_practice_basics/Hashing/hash_1.cpp
@@ -1,5 +1,35 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i = 0 ; i<n ; i++){
+//         cin>>arr[i];
+//     }
+
+// //precompute
+// int hash[13] = {0};
+// for(int i = 0 ; i<n ; i++){
+//     hash[arr[i]]++;
+// }
+
+//     int q; //no.of queries
+//     cin>>q;
+//     while (q--) //entering queries
+//     {
+//         int numbers;
+//         cin>>numbers;
+//         //fetch
+//         cout<<hash[numbers]<<" ";
+//     }
+    
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
+
 int main(){
     int n;
     cin>>n;
@@ -8,19 +38,19 @@ int main(){
         cin>>arr[i];
     }
 
-//precompute
-int hash[13] = {0};
-for(int i = 0 ; i<n ; i++){
-    hash[arr[i]]++;
-}
+    //precompute
+    int hash[13] = {0};
+    for(int i = 0 ; i<n; i++){
+        hash[arr[i]]++;
+    }
 
-    int q; //no.of queries
+
+    int q;
     cin>>q;
-    while (q--) //entering queries
+    while (q--)
     {
         int numbers;
         cin>>numbers;
-        //fetch
         cout<<hash[numbers]<<" ";
     }
     
